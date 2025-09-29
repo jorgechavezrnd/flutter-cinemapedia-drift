83be9f3f97bd5211f9389d4f2210a896336a70ce
jorgechavezrnd <jorgechavezrnd@gmail.com>
2025-09-29 17:13:56 -0400

    Add implementation for isFavoriteMovie and toggleFavoriteMovie

--- lib/infrastructure/datasources/drift_datasource.dart
+++ lib/infrastructure/datasources/drift_datasource.dart
@@ -1,11 +1,25 @@
+import 'package:drift/drift.dart' as drift;
+import 'package:cinemapedia/config/database/database.dart';
 import 'package:cinemapedia/domain/datasources/local_storage_datasource.dart';
 import 'package:cinemapedia/domain/entities/movie.dart';
 
 class DriftDatasource extends LocalStorageDatasource {
+  final AppDatabase database;
+
+  DriftDatasource([AppDatabase? databaseToUse])
+    : database = databaseToUse ?? db;
+
   @override
-  Future<bool> isFavoriteMovie(int movieId) {
-    // TODO: implement isFavoriteMovie
-    throw UnimplementedError();
+  Future<bool> isFavoriteMovie(int movieId) async {
+    // Construir el query
+    final query = database.select(database.favoriteMovies)
+      ..where((table) => table.movieId.equals(movieId));
+
+    // Ejecutar el query
+    final favoriteMovie = await query.getSingleOrNull();
+
+    // Retornar el resultado
+    return favoriteMovie != null;
   }
 
   @override
@@ -15,8 +29,28 @@ class DriftDatasource extends LocalStorageDatasource {
   }
 
   @override
-  Future<void> toggleFavoriteMovie(Movie movie) {
-    // TODO: implement toggleFavoriteMovie
-    throw UnimplementedError();
+  Future<void> toggleFavoriteMovie(Movie movie) async {
+    final isFavorite = await isFavoriteMovie(movie.id);
+
+    if (isFavorite) {
+      final deleteQuery = database.delete(database.favoriteMovies)
+        ..where((table) => table.movieId.equals(movie.id));
+
+      await deleteQuery.go();
+      return;
+    }
+
+    await database
+        .into(database.favoriteMovies)
+        .insert(
+          FavoriteMoviesCompanion.insert(
+            movieId: movie.id,
+            backdropPath: movie.backdropPath,
+            originalTitle: movie.originalTitle,
+            posterPath: movie.posterPath,
+            title: movie.title,
+            voteAverage: drift.Value(movie.voteAverage),
+          ),
+        );
   }
 }
