5c25447c7fb752bf7ddf2137f08f57fd9379c0ef
jorgechavezrnd <jorgechavezrnd@gmail.com>
2025-09-29 18:05:31 -0400

    Add method for toggle favorite movie in Favorite Movies Provider and use it in movie screen

--- lib/presentation/screens/movies/movie_screen.dart
+++ lib/presentation/screens/movies/movie_screen.dart
@@ -1,5 +1,6 @@
 import 'package:animate_do/animate_do.dart';
 import 'package:cinemapedia/presentation/providers/actors/actors_by_movie_provider.dart';
+import 'package:cinemapedia/presentation/providers/storage/favorite_movies_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -185,13 +186,13 @@ class _ActorsByMovie extends ConsumerWidget {
   }
 }
 
-class _CustomSliverAppBar extends StatelessWidget {
+class _CustomSliverAppBar extends ConsumerWidget {
   final Movie movie;
 
   const _CustomSliverAppBar({required this.movie});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     final size = MediaQuery.of(context).size;
 
     return SliverAppBar(
@@ -201,8 +202,10 @@ class _CustomSliverAppBar extends StatelessWidget {
 
       actions: [
         IconButton(
-          onPressed: () {
-            // TODO: implementar
+          onPressed: () async {
+            ref
+                .read(favoriteMoviesProvider.notifier)
+                .toggleFavoriteMovie(movie);
           },
           // icon: const Icon(Icons.favorite_border_outlined),
           icon: const Icon(Icons.favorite, color: Colors.red),
