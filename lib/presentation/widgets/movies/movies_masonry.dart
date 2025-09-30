8fa259d3a938bc4748986b252609b89092a1de62
jorgechavezrnd <jorgechavezrnd@gmail.com>
2025-09-29 20:13:30 -0400

    Add infinite scroll in favorites view

--- lib/presentation/widgets/movies/movies_masonry.dart
+++ lib/presentation/widgets/movies/movies_masonry.dart
@@ -16,10 +16,41 @@ class MovieMasonry extends StatefulWidget {
 class _MovieMasonryState extends State<MovieMasonry> {
   bool isLastPage = false;
   bool isLoading = false;
+  final scrollController = ScrollController();
 
   @override
   void initState() {
     super.initState();
+
+    scrollController.addListener(() {
+      // if (widget.loadNextPage == null) return;
+
+      // si el scroll está al final o cerca de unos 200px, aquí llamar el loadNextPage
+      if (scrollController.position.pixels + 200 >=
+          scrollController.position.maxScrollExtent) {
+        loadNextPageMovies();
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    scrollController.dispose();
+
+    super.dispose();
+  }
+
+  void loadNextPageMovies() async {
+    if (isLoading || isLastPage) return;
+    if (widget.loadNextPage == null) return;
+
+    isLoading = true;
+    final movies = await widget.loadNextPage!();
+    isLoading = false;
+
+    if (movies.isEmpty) {
+      isLastPage = true;
+    }
   }
 
   @override
@@ -27,6 +58,7 @@ class _MovieMasonryState extends State<MovieMasonry> {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 10),
       child: MasonryGridView.count(
+        controller: scrollController,
         crossAxisCount: 3,
         mainAxisSpacing: 10,
         crossAxisSpacing: 10,
